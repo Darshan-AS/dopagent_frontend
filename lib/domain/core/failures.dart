6cc197d2942111055da7e0eba58c94402ac3a41e
Darshan A S <itisdarshan@gmail.com>
2021-02-04 00:31:40 +0530

    Delete generated files

--- lib/domain/core/failures.dart
+++ lib/domain/core/failures.dart
@@ -6,10 +6,10 @@ part 'failures.freezed.dart';
 @freezed
 abstract class ValueFailure<T> with _$ValueFailure<T> {
   const factory ValueFailure.invalidEmail({
-    @required String invalidValue,
+    @required T invalidValue,
   }) = InvalidEmail<T>;
 
   const factory ValueFailure.shortPassword({
-    @required String invalidValue,
+    @required T invalidValue,
   }) = ShortPassword<T>;
 }
