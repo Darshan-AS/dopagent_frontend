64882943ad9cddfc05e3f0d3fac2e1f7599e9676
Darshan A S <itisdarshan@gmail.com>
2021-02-19 00:58:21 +0530

    Remove unnecessary files

--- lib/domain/accounts/account_failure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'account_failure.freezed.dart';
-
-@freezed
-abstract class AccountFailure with _$AccountFailure {
-  const factory AccountFailure.unexpected() = _Unexpected;
-}
--- lib/domain/scan/scan_code/scan_code_failure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'scan_code_failure.freezed.dart';
-
-@freezed
-abstract class ScanCodeFailure with _$ScanCodeFailure {
-  const factory ScanCodeFailure.unexpected() = _Unexpected;
-}
